aa90c9af8f9cfa823b1a2d544f63c593b633ba7a
gesslar <1266935+gesslar@users.noreply.github.com>
2026-04-01 19:29:59 -0400

    mew

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 
 export default defineConfig({
-  site: 'https://bedoc.gesslar.dev',
+  site: 'https://bedoc.gesslar.io',
   integrations: [
     starlight({
       title: 'BeDoc',
